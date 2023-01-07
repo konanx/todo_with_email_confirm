878b44be506d3c8af59f6edf6fa95af922309eee
konanx <micha112@onet.pl>
2023-01-07 06:15:26 +0100

    7jan

---
+++ server-todo/apka/Logowanie.ts
@@ -0,0 +1,10 @@
+import { LoginAttempt } from "./LogowanieInterfaces";
+
+// FUNKCJA WYWOLYWANA W MOMENCIE KLIKNIECIA PRZYCISKU ZALOGUJ PRZEZ UZYTKOWNIKA
+export const ProbaZalogowania = (data: LoginAttempt) => {
+  const { login, password } = data;
+};
+
+const GetAccountsList = async () => {
+  new Promise((resolve, reject) => {});
+};
