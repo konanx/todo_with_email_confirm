c65f3cca8a8598e33799c9a1d17ca21ebdc6dd88
konanx <micha112@onet.pl>
2023-01-08 21:29:38 +0100

    master

--- react-todo/src/components/Logowanie/Rejestracja.tsx
+++ react-todo/src/components/Logowanie/Rejestracja.tsx
@@ -15,39 +15,52 @@ function Rejestracja() {
         mt: 4,
       }}
     >
-      <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+      <Box
+        component="form"
+        autoComplete="off"
+        sx={{ display: "flex", flexDirection: "column", gap: 1 }}
+      >
         <Typography sx={{ textAlign: "center" }} variant="h6">
           REJESTRACJA
         </Typography>
         <TextField
+          autoComplete="off"
+          type="login"
           id="outlined-basic"
           label="Login"
           variant="outlined"
           value={login}
           onChange={(e) => setLogin(e.target.value)}
+          required
         />
         <TextField
+          type="email"
           id="outlined-basic"
           label="Email"
           variant="outlined"
-          value={login}
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
+          inputProps={{ inputMode: "email" }}
+          required
         />
         <TextField
           id="outlined-basic"
           label="Haslo"
           variant="outlined"
-          value={login}
+          value={password}
           onChange={(e) => setPassword(e.target.value)}
+          required
         />
         <TextField
           id="outlined-basic"
           label="Powtórz haslo"
           variant="outlined"
-          value={login}
+          value={password2}
           onChange={(e) => setPassword2(e.target.value)}
+          required
         />
         <Button
+          type="button"
           sx={{ mt: 1 }}
           variant="contained"
           endIcon={<SendIcon />}
