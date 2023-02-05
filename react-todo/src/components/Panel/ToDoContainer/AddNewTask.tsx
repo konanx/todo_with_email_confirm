0e793b30ce03d38ea803ad6d8a19d65118f2e46d
konanx <micha112@onet.pl>
2023-02-05 23:49:45 +0100

    master

--- react-todo/src/components/Panel/ToDoContainer/AddNewTask.tsx
+++ react-todo/src/components/Panel/ToDoContainer/AddNewTask.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
+import { Input, Space } from "antd";
+import { AudioOutlined } from "@ant-design/icons";
+
+const { Search } = Input;
+const suffix = (
+  <AudioOutlined
+    onClick={SpeechRecognition.startListening}
+    style={{
+      fontSize: 16,
+      color: "#1890ff",
+    }}
+  />
+);
 
 const AddNewTask = () => {
+  const [newTask, setNewTask] = useState();
   const {
     transcript,
     listening,
@@ -13,13 +27,25 @@ const AddNewTask = () => {
   if (!browserSupportsSpeechRecognition) {
     return <span>Twoja przeglądarka nie wspiera zamiany tekstu na mowę :</span>;
   }
+  useEffect(() => {
+    setNewTask(transcript);
+  }, [transcript]);
   return (
     <div>
-      <p>Microphone: {listening ? "on" : "off"}</p>
+      {/* <p>Microphone: {listening ? "on" : "off"}</p>
       <button onClick={SpeechRecognition.startListening}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <p>{transcript}</p> */}
+      <Search
+        // placeholder="input search text"
+        enterButton="Dodaj"
+        value={newTask}
+        onChange={(e: any) => setNewTask(e.target.value)}
+        size="large"
+        suffix={suffix}
+        // onSearch={onSearch}
+      />
     </div>
   );
 };
